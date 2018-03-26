e01b51428eac556d422b0f33a76249e81a40a489
Robson Adorno Ferrera <robson-macbook@MacBook-Pro-de-Robson.local>
2018-03-26 07:02:40 -0300

    Primeiro commit

---
+++ ExercicioOO/ExercicioOO/DAL/ClienteDAO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ExercicioOO.Model;
+using System.Collections.Generic;
+
+
+namespace ExercicioOO.DAL
+{
+    public class ClienteDAO
+    {
+
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public static List<Cliente> BuscarClientes()
+        {
+            foreach (Cliente clienteCadastrado in clientes)
+            {
+                Console.WriteLine(clienteCadastrado);
+            }
+            return clientes;
+        }
+
+        public static List<Cliente> CadastrarCliente(Cliente c){
+
+            clientes.Add(c);
+
+            return clientes;
+
+        }
+
+        public static List<Cliente> TrazerClientes()
+        {
+
+            return clientes;
+        }
+    }
+}
---
+++ ExercicioOO/ExercicioOO/View/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using ExercicioOO.Model;
+using ExercicioOO.DAL;
+
+namespace ExercicioOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente c;
+            string op;
+              do
+            {
+
+            Console.WriteLine("Por favor, digite a opção desejada logo abaixo:");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("1) Cadastrar Clientes.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("2) Listar Clientes.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("3) Cadastrar Vendendor.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("4) Listar Vendendor.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("5) Cadastrar Produtos.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("6) Listar Produtos.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("7) Registar Vendas.");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("8) Listar Vendas");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("9) Listar Vendas por Ciente");
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("0) Sair.");
+            op = Console.ReadLine();
+            Console.ReadKey();
+
+                switch (op)
+                {
+                    case "1":
+                        c = new Cliente();
+                        Console.WriteLine("Você selecionou a opção Cadastrar Clientes");
+                        Console.WriteLine("Digite o nome do Cliente");
+                        c.nome = Console.ReadLine();
+                        Console.WriteLine("Digite o CPF do Cliente");
+                        c.cpf = Console.ReadLine();
+                        ClienteDAO.CadastrarCliente(c);
+                        Console.WriteLine("Pressione algum botao para voltar ao menu");
+                        Console.ReadKey();
+                        break;
+                   
+                    case "2":
+                        Console.WriteLine("Você selecionou a opção Listar Clientes");
+                        foreach (Cliente ClienteCadsatrado in ClienteDAO.TrazerClientes())
+                        {
+                            Console.WriteLine("Esse eh o nome!!" + ClienteCadsatrado.nome);
+                            Console.WriteLine("Esse eh o seu cpf!!" +  ClienteCadsatrado.cpf);
+                        }
+                        Console.WriteLine("Pressione algum botao para voltar ao menu");
+                        Console.ReadKey();
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Saindo da aplicação...");
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida, digite novamente!");
+                        break;
+                }
+            } while (op != "0");
+
+        }
+    }
+}
